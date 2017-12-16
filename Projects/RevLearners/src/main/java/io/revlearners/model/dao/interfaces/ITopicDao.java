477266bdc9e2b2ee235309994590a05b9d47cc20
Matthew Snee <mjsnee@uno.edu>
2017-12-16 18:14:47 -0500

    updated beans

---
+++ Projects/RevLearners/src/main/java/io/revlearners/model/dao/interfaces/ITopicDao.java
@@ -0,0 +1,8 @@
+package io.revlearners.model.dao.interfaces;
+
+import io.revlearners.model.bean.Topic;
+import io.revlearners.util.persistence.interfaces.IGenericDao;
+
+public interface ITopicDao extends IGenericDao<Topic> {
+
+}
---
+++ Projects/RevLearners/src/main/java/io/revlearners/model/services/dao/hibernate/injectors/TopicHibernateDaoInjectorService.java
@@ -0,0 +1,24 @@
+package io.revlearners.model.services.dao.hibernate.injectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import io.revlearners.model.bean.Topic;
+import io.revlearners.model.dao.interfaces.ITopicDao;
+import io.revlearners.model.services.dao.interfaces.contracts.ITopicDaoInjectorService;
+import io.revlearners.util.commons.configs.Constants;
+import io.revlearners.util.persistence.hibernate.AbstractHibernateDaoInjectorService;
+import io.revlearners.util.persistence.interfaces.IGenericDao;
+
+@Service
+public class TopicHibernateDaoInjectorService extends AbstractHibernateDaoInjectorService<Topic> implements ITopicDaoInjectorService {
+	@Autowired
+	@Qualifier(Constants.QUALIFY_TOPIC_DAO)
+	private ITopicDao dao;
+	
+	@Override
+	protected IGenericDao<Topic> getDao() {
+		return dao;
+	}
+}
