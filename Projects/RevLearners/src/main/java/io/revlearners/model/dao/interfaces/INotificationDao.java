4cf5f052bc12bb6c7ce1542e9e6e61a162aa4d51
mnlwsn <mnlwsn@icloud.com>
2017-12-22 09:37:09 -0500

    notification dao/service/interface abstracted out into bean dao/...

--- Projects/RevLearners/src/main/java/io/revlearners/model/dao/interfaces/INotificationDao.java
+++
@@ -1,8 +0,0 @@
-package io.revlearners.model.dao.interfaces;
-
-import io.revlearners.model.bean.Notification;
-import io.revlearners.util.persistence.interfaces.IGenericDao;
-
-public interface INotificationDao extends IGenericDao<Notification> {
-
-}
--- Projects/RevLearners/src/main/java/io/revlearners/model/services/dao/interfaces/contracts/INotificationService.java
+++
@@ -1,8 +0,0 @@
-package io.revlearners.model.services.dao.interfaces.contracts;
-
-import io.revlearners.model.bean.Notification;
-import io.revlearners.util.persistence.interfaces.IGenericDao;
-
-public interface INotificationService extends IGenericDao<Notification>{
-
-}
