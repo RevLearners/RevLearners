5199958eba573d16e1de1ea0a3989852c09ba205
Matthew Snee <mjsnee@uno.edu>
2017-12-26 17:53:57 -0500

    adding rank controller implementation

---
+++ Projects/RevLearnerBoot/src/main/java/io/revlearners/model/services/interfaces/IRankService.java
@@ -0,0 +1,10 @@
+package io.revlearners.model.services.interfaces;
+
+import io.revlearners.model.bean.Rank;
+import io.revlearners.model.bo.RankBo;
+
+public interface IRankService extends ICrudService<Rank, RankBo>{
+
+}
+
+
