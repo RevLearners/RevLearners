4c27bfb877c10b5b3409d3cc1e8e57b5d75b191a
mnlwsn <mnlwsn@icloud.com>
2017-12-27 16:02:57 -0500

    Drafted notification service (pre-boot-impl)

---
+++ Projects/RevLearnerBoot/src/main/java/io/revlearners/model/services/NotificationService.java
@@ -0,0 +1,103 @@
+package io.revlearners.model.services;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.annotations.Fetch;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import io.revlearners.model.bean.Challenge;
+import io.revlearners.model.bean.MessageStatus;
+import io.revlearners.model.bean.Notification;
+import io.revlearners.model.bean.User;
+import io.revlearners.model.services.dao.interfaces.contracts.IBeanService;
+import io.revlearners.util.commons.configs.Constants;
+
+public class NotificationService {
+	@Autowired
+	protected SessionFactory sf;
+
+	@Autowired
+	@Qualifier(Constants.QUALIFY_BEAN_DAO_INJECTOR)
+	protected IBeanService beanService;
+
+	public Notification generateCertificationNotification(User user) {
+
+		Transaction tx = null;
+		Notification notif = new Notification();
+		LocalDateTime now = LocalDateTime.now();
+		
+		MessageStatus status = null; 
+
+		try (Session session = sf.openSession()) {
+			status.setName("unread");
+			notif.setTitle("Certification Accepted");
+			notif.setContents("Congratulations, " + user.getFirstName() + "! Your certification has been verified. You have now been updated to an advanced user.");
+			notif.setTime(now);
+			notif.setStatus(status);
+			
+			tx = session.beginTransaction();
+			session.save(notif);
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx!=null) tx.rollback();
+		}
+		return notif;
+	}
+
+	public Notification generateAdvancementNotification(User user) {
+
+		Notification notif = new Notification();
+		LocalDateTime now = LocalDateTime.now();
+		MessageStatus status = new MessageStatus();
+		status.setName("unread");
+
+		notif.setTitle("Rank Advancement");
+		notif.setContents("You have been updated from");
+		notif.setTime(now);
+		notif.setStatus(status);
+
+		return notif;
+	}
+
+	public Notification generateChallengeNotification(Challenge challenge, User sender) {
+		Transaction tx = null;
+		
+		Notification notif = new Notification();
+		Set<User> receivers = challenge.getUsers();
+		LocalDateTime now = LocalDateTime.now();
+		MessageStatus status = new MessageStatus();
+		status.setName("unread");
+
+		notif.setSender(sender);
+		notif.setReceivers(receivers);
+		notif.setTitle("Challenge Issued!");
+		notif.setContents("You have been issued a challenge by: " + sender);
+		notif.setTime(now);
+		notif.setStatus(status);
+
+		return notif;
+	}
+
+	// public void pushToDB(Notification notif) {
+	// //should be done by default upon creation?
+	// }
+
+	public Set<Notification> getByUserId(User user) {
+
+		Set<Notification> table = null;
+
+		return null;
+	}
+
+	public void updateStatus(Notification notif) {
+		MessageStatus status = notif.getStatus();
+
+	}
+
+}
