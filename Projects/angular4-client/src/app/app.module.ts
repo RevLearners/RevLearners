846b7454daf11ee4ba433994264b7ff5b3a6e4fc
pibe2 <pibe2@illinois.edu>
2017-12-27 14:55:00 -0500

    updating app.modules.ts

--- Projects/angular4-client/src/app/app.module.ts
+++ Projects/angular4-client/src/app/app.module.ts
@@ -1,36 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { RouterModule} from '@angular/router';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent} from './components/navbar/navbar.component';
-import { CreateAccountComponent} from './components/CreateAccount/CreateAccount.component';
-import { appRoutes} from './routing';
-import { HomeComponent } from './components/home/home.component';
-import { CreateAccountService } from './service/create-account-service';
-import { HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './components/Login/Login.component';
-import { CreateChallengeComponent } from './components/CreateChallenge/CreateChallenge.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {CreateAccountComponent} from './components/CreateAccount/CreateAccount.component';
+import {appRoutes} from './routing';
+import {HomeComponent} from './components/home/home.component';
+import {CreateAccountService} from './service/create-account-service';
+import {HttpClientModule} from '@angular/common/http';
+import {LoginComponent} from './components/Login/Login.component';
+import {CreateChallengeComponent} from './components/CreateChallenge/CreateChallenge.component';
+import {SubmitCertificationComponent} from './components/submit-certification/submit-certification.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    CreateAccountComponent,
-    HomeComponent,
-    LoginComponent,
-    CreateChallengeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes),
-    HttpClientModule,
-    ReactiveFormsModule
-  ],
-  providers: [CreateAccountService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        CreateAccountComponent,
+        HomeComponent,
+        LoginComponent,
+        CreateChallengeComponent,
+        SubmitCertificationComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot(appRoutes),
+        HttpClientModule,
+        ReactiveFormsModule
+    ],
+    providers: [CreateAccountService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
