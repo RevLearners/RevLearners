0cda94c65c696bcab42c70b3c8569cf815bb6b29
Loweski <lengvang9000@gmail.com>
2017-12-27 15:19:19 -0800

    fixed merge conflict

--- Projects/angular4-client/src/app/service/create-account-service.ts
+++ Projects/angular4-client/src/app/service/create-account-service.ts
@@ -8,32 +8,32 @@ export class CreateAccountService {
     constructor(private http: HttpClient) {
     }
 
-    public createAccount(userCred: string[]) {
-        const params = new FormData();
-        params.append('fname', userCred[0]);
-        params.append('mname', userCred[1]);
-        params.append('lname', userCred[2]);
-        params.append('username', userCred[3]);
-        params.append('password', userCred[4]);
-        params.append('email', userCred[5]);
-        console.log('Test2:' + userCred);
-        const options = {
+    public createAccount(userCred: string[]){
+        let data = new FormData();
+        data.append('fname', userCred[0]);
+        data.append('mname', userCred[1]);
+        data.append('lname', userCred[2]);
+        data.append('username', userCred[3]);
+        data.append('password', userCred[4]);
+        data.append('email', userCred[5]);
+        console.log("Test2:" + userCred);
+        let options = {
             headers: new HttpHeaders(),
         };
 
-        return this.http.post('http://localhost:8085/RevLearners/CreateAccount', params, options);
+        return this.http.post('http://localhost:8085/RevLearners/CreateAccount', data, options);
     }
 
     public userExist(username: string) {
 
-        const params = new FormData();
-        params.append('username', username);
+        const data = new FormData();
+        data.append('username', username);
 
         const options = {
             headers: new HttpHeaders(),
         };
 
         // Link will change after the rest calls/controller has been fully set up
-        return this.http.post('http://localhost:8085/RevLearners/???', params, options);
+        return this.http.post('http://localhost:8085/RevLearners/???', data, options);
     }
 }
