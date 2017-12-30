f08b1a9eab83c7ce235633dfdac62cef4fe7026c
Matthew Snee <mjsnee@uno.edu>
2017-12-29 19:13:34 -0500

    add drop-down template to navbar & implement some of ReportUser on the back end

--- Projects/RevLearnerBoot/src/main/java/io/revlearners/controller/routing/ReportController.java
+++ Projects/RevLearnerBoot/src/main/java/io/revlearners/controller/routing/ReportController.java
@@ -1,5 +1,41 @@
 package io.revlearners.controller.routing;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.revlearners.model.bo.ReportUserBo;
+
+import io.revlearners.util.commons.configs.WebConstants;
+
+@RestController
+@RequestMapping(WebConstants.REPORTS)
 public class ReportController  extends WebServicesController {
+	
+	@GetMapping(WebConstants.GET_BY_ID)
+	public ResponseEntity<ReportUserBo> getReportUserById(@PathVariable(value = "id") Long id) {
+		ReportUserBo report = serviceFacade.getReportUserById(id);
+		if(report == null)
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok().body(report);
+	}
+	
+	@GetMapping(WebConstants.GET_LIST)
+	public List<ReportUserBo> listTopics(Model model){
+		return serviceFacade.listReportUsers();
+	}
+	
+	@GetMapping(value = WebConstants.GET_PAGE, params = { "page", "size" })
+	public Page<ReportUserBo> pageTopics(Model model, @RequestParam("page") int page, @RequestParam("size") int size){
+		return serviceFacade.pageReportUsers(page, size);
+	}
 
 }
+
