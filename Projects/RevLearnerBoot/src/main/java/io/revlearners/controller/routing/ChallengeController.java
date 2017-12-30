0345ecdb752735176381d1fa80aac9c4ed502390
Loweski <lengvang9000@gmail.com>
2017-12-30 09:26:25 -0800

    updated ChallengeController

--- Projects/RevLearnerBoot/src/main/java/io/revlearners/controller/routing/ChallengeController.java
+++ Projects/RevLearnerBoot/src/main/java/io/revlearners/controller/routing/ChallengeController.java
@@ -1,5 +1,31 @@
 package io.revlearners.controller.routing;
 
+import java.util.List;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.revlearners.model.bean.Challenge;
+import io.revlearners.model.bo.ChallengeBo;
+import io.revlearners.util.commons.configs.WebConstants;
+
+@RestController
+@RequestMapping(WebConstants.CHALLENGES)
 public class ChallengeController extends WebServicesController {
+	
+	@GetMapping("/getChallenges/{challengeId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<ChallengeBo> getChallengesById(@PathVariable (value = "challengeId") int challengeId){
+		List<ChallengeBo> challenges = serviceFacade.listChallenges();
+		
+		
+		return challenges;
+	}
+	
 
 }
