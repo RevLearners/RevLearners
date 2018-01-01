09ff01d1a2d2f2ee60e9058f0ca05a877ee4958c
Loweski <lengvang9000@gmail.com>
2018-01-01 09:47:54 -0800

    updated leaderboard.component.ts

--- Projects/angular4-client/src/app/components/leaderboard/leaderboard.component.ts
+++ Projects/angular4-client/src/app/components/leaderboard/leaderboard.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import {User} from '../../model/user';
-import {Topic} from '../../model/topic';
+import { User } from '../../model/user';
+import { Topic } from '../../model/topic';
 
-import {LoginCredentialsService} from '../../services/login-credentials.service';
+import { LoginCredentialsService } from '../../services/login-credentials.service';
 import { BackendService } from '../../services/backend.service';
 
+import { AUTHORIZATION_HEADER, TOKEN_HEADER, SessionToken } from '../../model/session-token';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-leaderboard',
@@ -14,44 +16,60 @@ import { BackendService } from '../../services/backend.service';
   styleUrls: ['./leaderboard.component.css']
 })
 export class LeaderboardComponent implements OnInit {
-  
+
   user: User;
-  
+
   userScore: {
     topic: Topic,
     user: User,
     score: number
   }
-  
+
   //userScores = userScore[];
 
   users: User[];
   public topics = []
-  selectedTopic = '';
+  selectedTopic = 'Core Java';
 
-  constructor(private validate:LoginCredentialsService, private dataservice: BackendService) { }
+  token: SessionToken = null;
+
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private validate: LoginCredentialsService, private dataservice: BackendService) { }
 
   ngOnInit() {
-    this.user = this.validate.getUser();
+    console.log("IS THERE A TOKEN?")
+    console.log(this.token);
 
-    this.dataservice.getTopics().subscribe(
-      (data: any) =>{
+    this.user = this.validate.getUser();
+    this.token = this.validate.getToken();
+    if (this.user != null && this.token != null) {
+      this.headers = this.headers.append(AUTHORIZATION_HEADER, this.token.username);
+      this.headers = this.headers.append(TOKEN_HEADER, this.token.token);
+      this.dataservice.getTopics().subscribe(
+        (data: any) => {
           this.topics = data;
           console.log("Topic Data");
           console.log(data);
-      }
-  )
+        }
+      )
 
-    this.dataservice.getUsers().subscribe(
-      (data: any) =>{
+      this.dataservice.getUsers().subscribe(
+        (data: any) => {
           this.users = data;
           console.log("Data: " + data);
           console.log(data);
-      }
-  )
-
+        }
+      )
     }
-  
-  
+    console.log("Current User:");
+    console.log(this.user);
+
+
+  }
+
+  submit(value){
+    this.selectedTopic = value;
+  }
 
 }
